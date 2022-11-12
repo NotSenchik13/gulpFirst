497799ac1e00758b876f57a10e74aa46f72c530b
NotSenchik13 <websenchik@gmail.com>
2022-11-12 14:17:40 +0300

    Second comm

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+
+
 // Подключение модулей
 const gulp = require('gulp')
 const less = require('gulp-less')
@@ -7,6 +9,9 @@ const cleanCSS = require('gulp-clean-css')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 
 
 
@@ -19,6 +24,10 @@ const paths = {
     scripts: {
         src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/img/*',
+        dest: 'dist/img'
     }
 }
 
@@ -31,25 +40,43 @@ function clean() {
 
 // Задача для обработки стилей
 function styles() {
-    return gulp.src(paths.styles.src)        
+    return gulp.src(paths.styles.src)   
+        .pipe(sourcemaps.init())     
         .pipe(less())
-        .pipe(cleanCSS())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
         .pipe(rename({
             basename: 'main',
             suffix: '.min'
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.styles.dest))
 }
 
 // Задача для обработки скриптов
 function scripts() {
-    return gulp.src(paths.scripts.src, {
-        sourcemaps: true
-    })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    return gulp.src(paths.scripts.src)
+        .pipe(sourcemaps.init()) 
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.scripts.dest))
+}
+
+// Задача для компрессии изображений
+function img() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest(paths.images.dest))
 }
 
 // Задача для отслеживания изменений стилей и скриптов
@@ -59,7 +86,7 @@ function watch() {
 }
 
 // Задача для порядка выполнения сборки 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, gulp.parallel(styles, scripts, img), watch)
 
 // Экспорт задач
 exports.clean = clean
@@ -68,4 +95,4 @@ exports.scripts = scripts
 exports.watch = watch
 exports.build = build
 exports.default = build
-
+exports.img = img
---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+const b = 3;
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+class people {
+    say() {
+        console.log('Soup!')
+    }
+}
+const d = 3;
